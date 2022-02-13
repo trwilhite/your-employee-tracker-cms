da58489da0b40054c327db980fc4514c43a2cb81
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-02-12 22:50:09 -0500

    added quit and sleep functions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const Database = require('./lib/Database');
 
+// credit to Chris Backes - function to delay the prompt from reappearing until after the table functions have finished executing to improve user experience
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+
 const promptUser = async () => {
     let data = await inquirer.prompt([
         {
@@ -14,31 +17,42 @@ const promptUser = async () => {
             'Add a Department', 
             'Add a Role', 
             'Add an Employee', 
-            'Update an Employee Role'],
+            'Update an Employee Role',
+            'Quit'],
         }
     ]);
-    // respondUser(data.selections);
-    switch(data.selections) {
+    respondUser(data.selections);
+};
+
+const respondUser = async (selections) => {
+    switch(selections) {
         case 'View All Departments':
-            Database.viewDepartments();
+            await Database.viewDepartments();
             break;
         case 'View All Roles':
-            Database.viewRoles();
+            await Database.viewRoles();
             break;
         case 'View All Employees':
-            Database.viewEmployees();
+            await Database.viewEmployees();
             break;
         case 'Add a Department':
-            promptAddDepartment();
+            await promptAddDepartment();
             break;
-        // case 'Add a Role':
-        //     return addRole();
+        case 'Add a Role':
+            await promptAddRole();
+            break;
         // case 'Add an Employee':
         //     return addEmployee();
         // case 'Update an Employee Role':
         //     return updateEmployee();
+        case 'Quit':
+			console.log("You have successfully quit the application. Goodbye!");
+			await sleep();
+			return process.exit();
     }
-};
+    await sleep();
+    promptUser();
+}
 
 const promptAddDepartment = async () => {
     const department = await inquirer.prompt([
@@ -48,11 +62,40 @@ const promptAddDepartment = async () => {
             message: 'Enter the name of the department you would like to add:'
         },
     ]);
-    Database.addDepartment(department);
+    await Database.addDepartment(department);
     console.log(`Added ${department.name} to Departments!`)
 }
 
+// const promptAddRole = async () => {
+//     const [departments] = await Database.viewDepartments();
+
+//     const depts = departments.map(({ id, name }) => ({
+//         name: name,
+//         value: id,
+//     }));
+
+//     const role = await inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'title',
+//             message: 'Enter the title of the role you would like to add:'
+//         },
+//         {
+//             type: 'input',
+//             name: 'salary',
+//             message: 'Enter the salary for the role you would like to add:'
+//         },
+//         {
+//             type: 'list',
+//             name: 'department',
+//             message: 'What department does your new role belong to?',
+//             choices: depts
+//         }
+//     ]);
+//     await Database.addRole(role);
+//     console.log(`Added ${role.title} to Roles!`)
+// }
 
 promptUser();
 
-// module.exports = promptUser();
+// module.exports = promptUser;
